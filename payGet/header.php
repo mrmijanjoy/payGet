240d7fd9f9b753bc14f32957c636f4057e2cfa16
Mijanur Rahman Joy <mrmijanjoy@gmail.com>
2024-03-18 11:39:57 +0600

    Delete payGet directory

--- payGet/header.php
+++
@@ -1,8 +0,0 @@
-<?php  
-// remove comment // from the below code to enable errors & warnings for debugging
-// error_reporting(E_ALL);
-// ini_set('display_errors', 1);
-require __DIR__ . 'autoload.php';
-if (!defined('PAY_PAGE_CONFIG')) {
-    define('PAY_PAGE_CONFIG', realpath('config.php'));
-}
